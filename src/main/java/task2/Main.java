0f3ea500d7964f1865c63ae95259d0b97f8e8a61
Kuzin Dmitriy <SoraDnic@icloud.com>
2022-10-29 13:54:04 +0300

    initial commit

---
+++ src/main/java/task2/Main.java
@@ -0,0 +1,9 @@
+package task2;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1010", "1111"));
+        System.out.println(bins.mult("1010", "1111"));
+    }
+}
